84adcbaff3e7bcb7cbc6c8ce078d08cbe117b3f3
Lucas <ohpa_@hotmail.co.th>
2022-09-26 22:02:42 +0700

    add def and loop have problem in line 25                        (it's the bugtomorrow fix the bug)

--- f.py
+++ f.py
@@ -1,27 +1,61 @@
+import random
 import pygame
 from pygame.locals import *
 pygame.init()
-#กำหนดภาพ
+#Edit screen side
 f = pygame.RESIZABLE | DROPFILE | HWSURFACE
 screen = pygame.display.set_mode([800, 600],f)
 
-#ใส่ชื่อเกม
+#Name caption
 pygame.display.set_caption("Stupid game")
 #icon game
 logo = pygame.image.load('D:\c\img\starbucks_corporation_logo.png')
 pygame.display.set_icon(logo)
-#background and spaceship
+#background spaceship
 bg = pygame.image.load(r'D:\c\bg\768px-Milky_way.png')
 player = pygame.image.load('D:\c\img\spaceship\Starbucks-Cup.png')
+
+enemy = []
+bix = []
+biy = []
+cbix = []
+cbiy = []
+num_of_bi = 10
+
+for i in num_of_bi:
+    enemy.append(pygame.image.load('D:\c\img\Enemy\broccoli.png'))
+    bix.append(random.randint(0, 740))
+    biy.append(random.randint(0, 250))
+    cbix.append(random.randint(-1, 1))
+    cbiy.append(random.randint(1))
+
+def bi(x, y, i):
+    screen.blit(enemy[i], (x, y))
+
+
+
 #transform scale
 player = pygame.transform.scale(player,(100,100))
+#loop screen and end loop (Exit the program)
 t = True
 while t:
     for e in pygame.event.get():
         if e.type == QUIT:
             t = False
+#
 
     screen.blit(bg,(0, 0))
     screen.blit(player,(380, 520))
     pygame.display.flip()
+
+for i in range(num_of_bi):
+    bix[i] += cbix[i]
+    biy[i] += cbiy[i]
+    if biy[i] >= 600:
+       biy[i] = 0
+       bix[i] = random.randint(0 ,740)
+       bi(bix[i], biy[i], i)
+       pygame.display.update()
+
+
 
